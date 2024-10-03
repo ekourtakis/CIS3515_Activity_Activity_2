74f1565b141f856aadd288bd605fd4e122359bf2
ekourtakis <emmanuel.kourtakis@temple.edu>
2024-10-03 14:35:28 -0400

    completed TODO 1

--- app/src/main/java/edu/temple/activities/DisplayActivity.kt
+++ app/src/main/java/edu/temple/activities/DisplayActivity.kt
@@ -1,12 +1,22 @@
 package edu.temple.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
+
+const val EXTRA_KEY = "text size"
+
 
 class DisplayActivity : AppCompatActivity() {
+    val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        if (it.resultCode == RESULT_OK) {
+            it.data.apply {  }
+    }
 
+    }
     // TODO Step 1: Launch TextSizeActivity when button clicked to allow selection of text size value
 
     // TODO Step 3: Use returned value for lyricsDisplayTextView text size
@@ -21,5 +31,11 @@ class DisplayActivity : AppCompatActivity() {
         lyricsDisplayTextView = findViewById(R.id.lyricsDisplayTextView)
         textSizeSelectorButton = findViewById(R.id.textSizeSelectorButton)
 
+        textSizeSelectorButton.setOnClickListener {
+            val intent = Intent(this, TextSizeActivity::class.java)
+            launcher.launch(intent)
+        }
     }
+
+
 }
